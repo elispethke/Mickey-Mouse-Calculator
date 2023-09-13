5904b707b518ecb853b3adc55bb52deb75b8fb66
Elis Pethke <elispethke@gmail.com>
2023-09-13 23:22:29 +0200

    Initial Commit

---
+++ Mickey Mouse calculator/Mickey_Mouse_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mickey_Mouse_calculatorApp.swift
+//  Mickey Mouse calculator
+//
+//  Created by Elisangela Pethke on 13.09.23.
+//
+
+import SwiftUI
+
+@main
+struct Mickey_Mouse_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
